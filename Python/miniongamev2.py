21ba9f1ecb3ad5f04c9bd1d2e728040076b4faa1
AlanP <aln.10zepeda@gmail.com>
2022-06-27 21:01:54 -0500

    minion optimized but still getting time errors in 12+

--- Python/miniongamev2.py
+++ Python/miniongamev2.py
@@ -1,20 +1,17 @@
 #Code passed test excep optimization tests
+#Optimized, only focuse on outputing the output no extra diccionaries
+#With these method we are counting each occurrance, iterating all over the string, so adding an extra letter ath the end will increase two extra points.
 
 def minion_game(string):
     # your code goes here
     vowels = ["A","E","I","O","U"]
     i=0
-    kevin_w ={}
-    stuart_w ={}
-
     count_k =0
     count_s = 0
-
     for letter in string:
+        
         Kevin = False
         Stuart = False
-        subs=""
-        
         if (letter in vowels):
             Kevin = True
            
@@ -22,37 +19,21 @@ def minion_game(string):
             Stuart = True
 
         for other_string in string[i:]: #We take only the resting part of string
-            subs = subs+other_string
-            
             if Kevin == True:
-                if subs not in kevin_w:
-                    kevin_w[subs]=1
-                elif subs in kevin_w:
-                    kevin_w[subs]=1 +kevin_w[subs]
-
-            elif Stuart == True:
-                if subs not in stuart_w:
-                    stuart_w[subs] = 1
-                elif subs in stuart_w:
-                    stuart_w[subs] = 1 +stuart_w[subs]
-
-            
-        i+=1
-    for kevin in kevin_w:
-        count_k = count_k +kevin_w[kevin]
-
-    for stuart in stuart_w:
-        count_s = count_s +stuart_w[stuart]
+                count_k = count_k +1
     
+            elif Stuart == True:
+                
+                count_s = count_s +1
+        i+=1
     if count_k > count_s:
         print("Kevin " + str(count_k))
     elif count_s > count_k:
         print("Stuart " + str(count_s))
     else:
         print("Draw")
-
             
 
 if __name__ == '__main__':
-    s = "BANANA"
+    s = "BANANAS"
     minion_game(s)
